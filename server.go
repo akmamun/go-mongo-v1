be3b5c1e12460fa2fd96d7689f2f6761687609cb
mrxmamun <almamunkhan04@gmial.com>
2019-06-14 02:43:27 +0600

    fixed gin router issue

--- server.go
+++ server.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-mongo/routes"
+	"github.com/go-mongo/controllers"
+	"gopkg.in/mgo.v2"
 )
 
-var router *gin.Engine
+var todosCollection *mgo.Collection
+var session *mgo.Session
 
+var router *gin.Engine
+func init() {
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	session.SetMode(mgo.Monotonic, true)
+
+	// get a Collection of todo
+	todosCollection = session.DB("test-todo").C("todo")
+}
 func main() {
-	// router = gin.Default()
-	// Initialize the routes
-	routes.TodoRoute()
+	router := gin.Default()
+ 
+	v1 := router.Group("/api/v1/todos")
+	{
+		v1.POST("/", controllers.CreateTodo)
+		v1.GET("/", controllers.FetchAllTodo)
+		v1.GET("/:id", controllers.FetchSingleTodo)
+		v1.PUT("/:id", controllers.UpdateTodo)
+		v1.DELETE("/:id", controllers.DeleteTodo)
+	}
 
 	// Start serving the application
 	router.Run(":8000")
 
 }
+ 
+
+
+ 
